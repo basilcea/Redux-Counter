32c3eade90ac3932367fcc85de887c14569af6dd
basilcea <ogbonna.basil3@gmmail.com>
2019-06-17 13:21:36 +0100

    initial commit - add increment and decrement functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+
+
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
@@ -10,9 +12,9 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
-  // Fill in this function
+  return {type: INCREMENT}
 };
 
 export const decrement = () => {
-  // Fill in this function
+  return {type: DECREMENT}
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { INCREMENT, DECREMENT } from '../actions';
-
 const initialState = {
   count: 0
 }
@@ -12,9 +11,9 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return {count:state.count+1}
     case DECREMENT:
-    // Fill in the body of this case
+      return {count:state.count -1}
     default:
       return state;
   }
